fab48dbf3ea77c3fe0d4aaecfad3060e92a19748
Ishan Nigam <63084385+nigamishan@users.noreply.github.com>
2020-07-04 15:32:45 +0530

    Add files via upload

---
+++ scripts/home_service.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/ishan/catkin_ws/src/map/Ishan.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/ishan/catkin_ws/src/map/map.yaml
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 8
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects" &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers"
+
